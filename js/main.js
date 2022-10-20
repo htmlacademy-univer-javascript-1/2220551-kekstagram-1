c5b521d9b8c0c3a3a464869dfa58d8959cb3e5c5
Антон <stolbov.111@mail.ru>
2022-10-20 23:19:00 +0500

    Исправил ошибки в оформлении

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const NumberComments = {
   MAX: 5
 };
 
+
 const getRandomPositiveInclusive = (min, max) => {
   min = Math.ceil(Math.min(Math.abs(max), Math.abs(min)));
   max = Math.floor(Math.max(Math.abs(max), Math.abs(min)));
@@ -25,18 +26,18 @@ getRandomPositiveInclusive();
 сheckMaxLengthString();
 
 const getRandomName = () => {
-  const names = ['Антон', 'Максим', 'Геннадий', 'Юлия', 'Наталья', 'Григорий', 'Михаил', 'Роман', 'Анастасия', 'Дмитрий'];
-  return names[getRandomPositiveInclusive(0, names.length - 1)];
+  const NAMES = ['Антон', 'Максим', 'Геннадий', 'Юлия', 'Наталья', 'Григорий', 'Михаил', 'Роман', 'Анастасия', 'Дмитрий'];
+  return NAMES[getRandomPositiveInclusive(0, NAMES.length - 1)];
 };
 
 const getRandomComment = () => {
-  const comment = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-  return comment[getRandomPositiveInclusive(0, comment.length - 1)];
+  const COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  return COMMENTS[getRandomPositiveInclusive(0, COMMENTS.length - 1)];
 };
 
 const getDescription = () => {
-  const description = ['Здеась я кайфую', 'Здесь я работаю', 'Так нравится это фото', 'Прекрасно', 'Кайфули', 'Поставьте лайк пж'];
-  return description[getRandomPositiveInclusive(0, description.length - 1)];
+  const DESCTIPTION = ['Здеась я кайфую', 'Здесь я работаю', 'Так нравится это фото', 'Прекрасно', 'Кайфули', 'Поставьте лайк пж'];
+  return DESCTIPTION[getRandomPositiveInclusive(0, DESCTIPTION.length - 1)];
 };
 
 const makeComment = (id) => ({
@@ -58,4 +59,4 @@ const makePhoto = (id) => ({
 
 const similarPosts = Array.from({ length: MAX_COUNT_PHOTOS }).map((value, index) => makePhoto(index + 1)
 );
-console.log(similarPosts);
+console.log('Ваня коссе гей')
